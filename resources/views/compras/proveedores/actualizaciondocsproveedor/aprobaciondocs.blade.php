ae10560b67be1d9b261f048eb6ef71ed1efb2a75
andres69542 <andres69542@gmail.com>
2026-09-03 10:23:46 -0600

    Update aprobaciondocs.blade.php

--- resources/views/compras/proveedores/actualizaciondocsproveedor/aprobaciondocs.blade.php
+++ resources/views/compras/proveedores/actualizaciondocsproveedor/aprobaciondocs.blade.php
@@ -1,23 +1,16 @@
 @extends('principal.maestracompras')
-
 @section('contenido')
-
 
 
 <div class="contenedor-contenido">
     <ol class="breadcrumb mb-5" style="display: flex; justify-content: center; align-items: center;">
-        <h3 style="color: #ffffff; margin: 0;"><i class="bi bi-briefcase-fill"></i>&nbsp;Aprobación de documentos del colaborador</h3>
+        <h3 style="color: #ffffff; margin: 0;"><i class="bi bi-briefcase-fill"></i>&nbsp;Aprobación de documentos del proveedor </h3>
     </ol>
-
-
     <div class="card-body">
         <table id="Tabladocumentosaprobacionproveedor" class="table table-hover bg-white table-bordered text-center w-100 TableCustom">
         </table>
     </div>
-
 </div>
-
-
 
 
 @endsection
